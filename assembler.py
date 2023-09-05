59ed1f76be4e0cdc0939e33fb819a7843c339ad6
Elias Cregård <eliascregard701@gmail.com>
2023-09-05 23:05:20 +0200

    First commit

---
+++ assembler.py
@@ -0,0 +1,243 @@
+instructions = [
+    "NOP",
+    "JCN",
+    "FIM",
+    "SRC",
+    "FIN",
+    "JIN",
+    "JUN",
+    "JMS",
+    "INC",
+    "ISZ",
+    "ADD",
+    "SUB",
+    "LD",
+    "XCH",
+    "BBL",
+    "LDM",
+    "WRM",
+    "WMP",
+    "WRR",
+    "WR0",
+    "WR1",
+    "WR2",
+    "WR3",
+    "SBM",
+    "RDM",
+    "RDR",
+    "ADM",
+    "RD0",
+    "RD1",
+    "RD2",
+    "RD3",
+    "CLB",
+    "CLC",
+    "IAC",
+    "CMC",
+    "CMA",
+    "RAL",
+    "RAR",
+    "TCC",
+    "DAC",
+    "TCS",
+    "STC",
+    "DAA",
+    "KBP",
+    "DCL"
+]
+
+two_word_instructions = [
+    "JCN",
+    "FIM",
+    "JUN",
+    "JMS",
+    "ISZ",
+]
+
+data: list[int] = []
+labels: dict[str, int] = {}
+label_address_counter = 0
+
+
+def handle_lines(line: str) -> bool:
+    line.strip()
+    tokens = line.split(';')[0].replace("\t", " ")
+    if len(sp := tokens.split(",")) != 1:
+        tokens = sp[1].split(" ")
+    else:
+        tokens = tokens.strip().split(" ")
+    tokens = trim(tokens)
+    instruction = tokens[0].strip()
+    tokens = list(map(str.strip, tokens[1:]))
+    match instruction:
+        case "NOP":
+            data.append(0x00)
+        case "JCN":
+            data.append(0x10 + conv_int(tokens[0], 0xF))
+            data.append(labels[tokens[1]])
+        case "FIM":
+            data.append(0x20 + conv_int(tokens[0], 0x8) << 1)
+            data.append(conv_int(tokens[1]))
+        case "SRC":
+            data.append(0x21 + conv_int(tokens[0], 0x8) << 1)
+        case "FIN":
+            data.append(0x30 + conv_int(tokens[0], 0x8) << 1)
+        case "JIN":
+            data.append(0x31 + conv_int(tokens[0], 0x8) << 1)
+        case "JUN":
+            address = labels[tokens[0]]
+            data.append(0x40 + (address >> 8))
+            data.append(address & 0xFF)
+        case "JMS":
+            address = labels[tokens[0]]
+            data.append(0x50 + address >> 8)
+            data.append(address & 0xFF)
+        case "INC":
+            data.append(0x60 + conv_int(tokens[0], 0xF))
+        case "ISZ":
+            data.append(0x70 + conv_int(tokens[0], 0xF))
+            data.append(labels[tokens[1]] & 0xFF)
+        case "ADD":
+            data.append(0x80 + conv_int(tokens[0], 0xF))
+        case "SUB":
+            data.append(0x90 + conv_int(tokens[0], 0xF))
+        case "LD":
+            data.append(0xA0 + conv_int(tokens[0], 0xF))
+        case "XCH":
+            data.append(0xB0 + conv_int(tokens[0], 0xF))
+        case "BBL":
+            data.append(0xC0 + conv_int(tokens[0], 0xF))
+        case "LDM":
+            data.append(0xD0 + conv_int(tokens[0], 0xF))
+        case "WRM":
+            data.append(0xE0)
+        case "WMP":
+            data.append(0xE1)
+        case "WRR":
+            data.append(0xE2)
+        case "WPM":
+            data.append(0xE3)
+        case "WR0":
+            data.append(0xE4)
+        case "WR1":
+            data.append(0xE5)
+        case "WR2":
+            data.append(0xE6)
+        case "WR3":
+            data.append(0xE7)
+        case "SBM":
+            data.append(0xE8)
+        case "RDM":
+            data.append(0xE9)
+        case "RDR":
+            data.append(0xEA)
+        case "ADM":
+            data.append(0xEB)
+        case "RD0":
+            data.append(0xEC)
+        case "RD1":
+            data.append(0xED)
+        case "RD2":
+            data.append(0xEE)
+        case "RD3":
+            data.append(0xEF)
+        case "CLB":
+            data.append(0xF0)
+        case "CLC":
+            data.append(0xF1)
+        case "IAC":
+            data.append(0xF2)
+        case "CMC":
+            data.append(0xF3)
+        case "CMA":
+            data.append(0xF4)
+        case "RAL":
+            data.append(0xF5)
+        case "RAR":
+            data.append(0xF6)
+        case "TCC":
+            data.append(0xF7)
+        case "DAC":
+            data.append(0xF8)
+        case "TCS":
+            data.append(0xF9)
+        case "STC":
+            data.append(0xFA)
+        case "DAA":
+            data.append(0xFB)
+        case "KBP":
+            data.append(0xFC)
+        case "DCL":
+            data.append(0xFD)
+        case _:
+            print(f"Error: {instruction} is not a valid instruction")
+            return False
+    return True
+
+
+def add_label(line: str) -> bool:
+    global label_address_counter
+    line = line.replace("\t", " ")
+    label_address_counter += 1
+    line = line.strip().split(",")
+    if len(line) < 2:
+        sp = line[0].split(" ")
+        sp = trim(sp)
+        if sp[0] in two_word_instructions:
+            label_address_counter += 1
+        return True
+    elif len(line) > 2:
+        print("Error: Invalid line")
+        return False
+    if (line[0] in instructions) or (line[0] in labels):
+        return False
+    if line[0].isnumeric():
+        return False
+    labels[line[0]] = label_address_counter-1
+    return True
+
+
+def conv_int(num: str, size_limit: int = 0xFF) -> int:
+    len_num = len(num)
+    if len_num > 2 and num[:2] == "0x":
+        dat = int(num, 16)
+    elif len_num > 2 and num[:2] == "0b":
+        dat = int(num, 2)
+    else:
+        dat = int(num, 10)
+    if 0 <= dat <= size_limit:
+        return dat
+    else:
+        print(f"Error: {num} is out of range")
+        raise ValueError
+
+
+def trim(tokens: list[str]) -> list[str]:
+    i = 0
+    while i < len(tokens):
+        if tokens[i] == "":
+            tokens.pop(i)
+        else:
+            i += 1
+    return tokens
+
+
+def main():
+    with open("test.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if not add_label(line):
+                print("Error: Invalid line")
+                break
+        for line in lines:
+            if not handle_lines(line):
+                print("Error: Invalid line")
+                break
+    print(data)
+    with open("test.bin", "wb") as f:
+        f.write(bytearray(data))
+
+
+if __name__ == "__main__":
+    print()
+    main()
---
+++ main.cpp
@@ -0,0 +1,376 @@
+// http://datasheets.chipdb.org/Intel/MCS-4/datashts/intel-4004.pdf
+// http://e4004.szyc.org/iset.html
+// https://www.inf.pucrs.br/~calazans/undergrad/orgcomp_EC/mat_microproc/Intel-4004_InstructionSet.pdf
+// http://e4004.szyc.org/asm.html
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+
+using word = unsigned char;
+using byte = unsigned char;
+using chunk64 = unsigned long long;
+
+class ROM {
+    int byteSize;
+protected:
+    word *memory;
+public:
+    explicit ROM(word* memory) {
+        this->memory = memory;
+        this->byteSize = sizeof(this->memory);
+    }
+    word fetch(unsigned short address)
+    {
+        if (address > 255) return '\0';
+        return memory[address];
+    }
+};
+
+class RAM {
+public:
+    byte memory[40];
+    byte fetch(unsigned short address) {
+        if (address > 39) return '\0';
+        return memory[address];
+    }
+    void store(unsigned short address, byte data) {
+        if (address > 39) return;
+        memory[address] = data;
+    }
+
+};
+
+class CPU {
+
+    chunk64 REGISTERS = 0;  // R15 least significant, R0 most significant
+    chunk64 C         : 1;
+    chunk64 TEST      : 1;
+    chunk64 ACC       : 4;
+    chunk64 OPA       : 4;
+    chunk64 OPR       : 4;
+    chunk64 PC        : 12;
+    chunk64 PC1       : 12;
+    chunk64 PC2       : 12;
+    chunk64 PC3       : 12;
+    ROM* programRom;
+    RAM* dataRam;
+
+
+    // instruction set
+    constexpr static word NOP = 0x00;
+    constexpr static word JCN = 0x10;
+    constexpr static word FIM = 0x20;
+    constexpr static word FIN = 0x30;
+    constexpr static word JIN = 0x40;
+    constexpr static word JUN = 0x50;
+    constexpr static word JMS = 0x60;
+    constexpr static word INC = 0x70;
+    constexpr static word ISZ = 0x80;
+    constexpr static word ADD = 0x90;
+    constexpr static word SUB = 0xA0;
+    constexpr static word LD  = 0xB0;
+    constexpr static word XCH = 0xC0;
+    constexpr static word BBL = 0xD0;
+    constexpr static word LDM = 0xE0;
+    constexpr static word CLB = 0xF0;
+    constexpr static word CLC = 0xF1;
+    constexpr static word IAC = 0xF2;
+    constexpr static word CMC = 0xF3;
+    constexpr static word CMA = 0xF4;
+    constexpr static word RAL = 0xF5;
+    constexpr static word RAR = 0xF6;
+    constexpr static word TCC = 0xF7;
+    constexpr static word DAC = 0xF8;
+    constexpr static word TCS = 0xF9;
+    constexpr static word STC = 0xFA;
+    constexpr static word DAA = 0xFB;
+    constexpr static word KBP = 0xFC;
+    constexpr static word DCL = 0xFD;
+public:
+    void runProgram(int cycles, unsigned short startAddress) {
+        PC = startAddress;
+        word instruction;
+        word C0;
+        while (cycles > 0) {
+            instruction = programRom->fetch(PC);
+            OPR = instruction >> 4;
+            OPA = instruction & 0x0F;
+            if (OPR == 0xF) {
+                switch (OPA) {
+                    case 0b0000:
+                        ACC = C = 0;
+                        break;
+                    case 0b0001:
+                        C = 0;
+                        break;
+                    case 0b0010:
+                        ACC++;
+                        if (ACC == 0) C = 1;
+                        break;
+                    case 0b0011:
+                        C = ~C;
+                        break;
+                    case 0b0100:
+                        ACC = ~ACC;
+                        break;
+                    case 0b0101:
+                        C0 = C;
+                        C = ACC >> 3;
+                        ACC = ACC << 1;
+                        ACC += C0;
+                        break;
+                    case 0b0110:
+                        C0 = C;
+                        C = ACC & 0b1;
+                        ACC = ACC >> 1;
+                        ACC += C0 << 3;
+                        break;
+                    case 0b0111:
+                        ACC = C;
+                        C = 0;
+                        break;
+                    case 0b1000:
+                        if (ACC == 0) C = 1;
+                        ACC--;
+                        break;
+                    case 0b1001:
+                        if (C == 0) ACC = 0b1001;
+                        else ACC = 0b1010;
+                        C = 0;
+                        break;
+                    case 0b1010:
+                        C = 1;
+                        break;
+                    case 0b1011:
+                        if (ACC > 0b1001 || C == 1) ACC += 0b0110;
+                        if (ACC < 0b0110) C = 1;
+                        break;
+                    case 0b1100:
+                        if (!(ACC == 0 || ACC == 1 || ACC == 2 || ACC == 4 || ACC == 8)) ACC = 0xF;
+                        break;
+                    case 0b1101:
+                        break;
+                    default:
+                        break;
+                }
+                PC++;
+                cycles--;
+            } else {
+                bool jump;
+                word reg, regPair, wAddress;
+                short sAddress;
+                chunk64 regMask;
+                unsigned short pcMask;
+                switch (OPR) {
+                    case 0x0:
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0x1:
+                        jump = false;
+                        if (((OPA & 0b0100) == 0b0100 && ACC == 0) ||
+                            ((OPA & 0b0010) == 0b0010 && C == 1) ||
+                            ((OPA & 0b0001) == 0b0001 && TEST == 0)) jump = true;
+                        if ((OPA & 0b1000) == 0b1000) jump = !jump;
+                        if (jump) {
+                            PC = programRom->fetch(++PC);
+                        } else {
+                            PC += 2;
+                        }
+                        cycles -= 2;
+                        break;
+                    case 0x2:
+                        if ((OPA & 0b0001) == 0b0000) {
+                            regPair = OPA >> 1;
+                            regMask = 0xFF << (15 - regPair * 8);
+                            REGISTERS |= regMask;
+                            regMask = ~programRom->fetch(++PC) << (15 - regPair * 8);
+                            REGISTERS &= ~regMask;
+                            PC++;
+                            cycles--;
+                        } else {
+                            // Instruction: SRC
+                        }
+                        cycles--;
+                        break;
+//                    case 0x3:
+//                        if ((OPA & 0b0001) == 0b0000) {
+//
+//                        } else {
+//                            regPair = OPA >> 1;
+//                            PC |= 0xFF;
+//                            pcMask = ~((word) (REGISTERS >> (15 - regPair * 8)));
+//                            pcMask = ~pcMask;
+//                            PC &= pcMask;
+//                        }
+
+                    case 0x4:
+                        sAddress = ((short) OPA << 8) + programRom->fetch(PC + 1);
+                        PC = sAddress;
+                        cycles -= 2;
+                        break;
+                    case 0x5:
+                        PC3 = PC2;
+                        PC2 = PC1;
+                        PC1 = PC+1;
+                        PC = (OPA << 8) + programRom->fetch(PC + 1);
+                        cycles -= 2;
+                    case 0x6:
+                        reg = ((REGISTERS >> (15 - OPA * 4)) << 4) >> 4;
+                        if (reg == 15) {
+                            REGISTERS &= ~((chunk64)(0xF << (15 - OPA * 4)));
+                        } else {
+                            REGISTERS += 1 << (15 - OPA * 4);
+                        }
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0x7:
+                        reg = ((REGISTERS >> (15 - OPA * 4)) << 4) >> 4;
+                        if (reg == 15) {
+                            REGISTERS &= ~((chunk64)(0xF << (15 - OPA * 4)));
+                            PC++;
+                        } else {
+                            REGISTERS += 1 << (15 - OPA * 4);
+                            PC = programRom->fetch(PC + 1);
+                        }
+                        cycles -= 2;
+                        break;
+                    case 0x8:
+                        reg = ((REGISTERS >> (15 - OPA * 4)) << 4) >> 4;
+                        ACC += reg;
+                        if (ACC < reg) C = 1;
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0x9:
+                        reg = -((REGISTERS >> (15 - OPA * 4)) << 4) >> 4;
+                        reg += ACC + C;
+                        if (reg >= 16) C = 0;
+                        ACC = reg;
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0xA:
+                        ACC = REGISTERS >> (15 - OPA * 4);
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0xB:
+                        pcMask = ACC; //tempACC
+                        ACC = REGISTERS >> (15 - OPA * 4);
+                        regMask = 0xF << (15 - OPA * 4);
+                        REGISTERS |= regMask;
+                        regMask = ~pcMask << (15 - OPA * 4);
+                        REGISTERS &= ~regMask;
+                        PC++;
+                        cycles--;
+                        break;
+                    case 0xC:
+                        ACC = OPA;
+                        PC = PC1;
+                        PC1 = PC2;
+                        PC2 = PC3;
+                        cycles--;
+                    case 0x0D:
+                        ACC = OPA;
+                        PC++;
+                        cycles--;
+                        break;
+                }
+            }
+        }
+    }
+
+public:
+    CPU() = default;
+    CPU(RAM* dataRam)
+    {
+        this->dataRam = dataRam;
+    }
+    CPU(ROM* programRom)
+    {
+        this->programRom = programRom;
+    }
+    CPU(RAM* dataRam, ROM* programRom)
+    {
+        this->dataRam = dataRam;
+        this->programRom = programRom;
+    }
+    void connectDataRam(RAM* memory) {
+        dataRam = memory;
+    }
+    void connectProgramRom(ROM* memory) {
+        programRom = memory;
+    }
+
+};
+
+constexpr char wordToChar[16] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                 'A', 'B', 'C', 'D', 'E', 'F'};
+
+//void generateProgram(char* path, word* characters) {
+//    int numOfCharacters = 0;
+//    std::ifstream stream(path);
+//    std::string line;
+//    if (!stream.is_open()) {
+//        std::cout << "Unable to open file";
+//        exit(1);
+//    }
+//    while (std::getline(stream, line)) {
+//        numOfCharacters += line.length();
+//    }
+//    stream.close();
+//    if (numOfCharacters % 2 == 1) {
+//        numOfCharacters = numOfCharacters / 2 + 1;
+//    } else {
+//        numOfCharacters = numOfCharacters / 2;
+//    }
+//    characters = new word[numOfCharacters];
+//    stream.open(path);
+//    std::map<char, word> charToWord;
+//    for (int i = 0; i < 16; i++) {
+//        charToWord[wordToChar[i]] = i;
+//    }
+//    int i = 0;
+//    char c;
+//    while (stream >> c) {
+//        word byte = charToWord[c];
+//        if (stream >> c) {
+//            byte = byte << 4;
+//            byte += charToWord[c];
+//        }
+//        characters[i++] = byte;
+//    }
+//}
+
+int sizeOfFile(char* path) {
+    std::ifstream in_file(path, std::ios::binary);
+    in_file.seekg(0, std::ios::end);
+    return in_file.tellg();
+}
+
+void loadProgram(char* path, word* characters) {
+    std::ifstream file(path, std::ios::in | std::ios::binary);
+    file.read((char*) characters, sizeOfFile(path));
+    file.close();
+}
+
+int main() {
+    auto* dataMemory = new RAM();
+//    word programMemArray[256] {0x20, 0x00, 0xF0, 0xF2, 0x16, 0x08, 0x40, 0x03, 0x60, 0x40, 0x03};
+    word programMemArray[256];
+    for (word & i : programMemArray) {
+        i = 0;
+    }
+    loadProgram("C:\\Users\\elias\\CLionProjects\\Intel 4004 Emulator\\test.bin", programMemArray);
+    auto* programMemory = new ROM(programMemArray);
+    auto* cpu = new CPU(dataMemory, programMemory);
+    cpu->runProgram(5000000, 0);
+
+    return 0;
+}
